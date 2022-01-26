c8416942b3e48a1485da76a6f369b1fe113a89e7
recepdur <recepdur@outlook.com>
2022-01-26 23:19:46 +0300

    axios example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+const axios = require("axios");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.login = this.login.bind(this);
+  }
+
+  login() {
+    axios.defaults.withCredentials = true;
+    //axios.defaults.baseURL = "http://localhost:8000";
+    axios.defaults.baseURL = "https://crm-laravel-api.herokuapp.com";
+
+    axios.get("/sanctum/csrf-cookie").then((response) => {
+      console.log(response);
+      axios
+        .post("/api/login", {
+          email: "recep_1@gmail.com",
+          password: "123456",
+        })
+        .then((response2) => {
+          console.log(response2);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.login}>Login</button>
+      </div>
+    );
+  }
 }
 
 export default App;
